80be9ed4c7300f7109ebeb07eb0431f7d35f9963
luffyZhou <zhoudeyou945@126.com>
2020-01-13 17:29:54 +0800

    add gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react")
+// Adds a class name to the body element
+exports.onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
+  setBodyAttributes({
+    className: "my-body-class",
+  })
+};
+
+// Move Typography.js styles to the top of the head section so they're loaded first.
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents();
+  headComponents.sort((x, y) => {
+    if (x.key === 'TypographyStyle') {
+      return -1
+    } else if (y.key === 'TypographyStyle') {
+      return 1
+    }
+    return 0
+  })
+  replaceHeadComponents(headComponents)
+}
